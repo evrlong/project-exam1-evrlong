c978c6a8ac63b64b00b23039bb0a7559d079cbe5
evrlong <127796398+evrlong@users.noreply.github.com>
2024-11-09 21:53:54 +0100

    disable btn

--- js/components/renderTenCards.js
+++ js/components/renderTenCards.js
@@ -1,5 +1,3 @@
-//renderTenCards.js: This file will be used to render the blog cards on the blogs.html page.
-
 import { fetchData } from '../data/fetchDataWithMedia.js'; // Path to fetchData
 
 let currentPage = 1; // Start at the first page
@@ -8,12 +6,14 @@ let posts = []; // To store all posts
 
 const loader = document.getElementById('loader');
 const loadedContent = document.querySelector('.loadedContent');
+const showMoreBtn = document.querySelector('.showMoreBtn'); // "Show More" button
 
 export async function renderInitialBlogCards() {
   await renderBlogCards();
 }
 
 export async function showMoreBlogCards() {
+  console.log('Show more blog cards', currentPage);
   currentPage++;
   await renderBlogCards();
 }
@@ -22,16 +22,35 @@ async function renderBlogCards() {
   try {
     loader.style.display = "block";
     loadedContent.style.display = "none";
+
     // Fetch posts for the current page
     const fetchedPosts = await fetchData(postsPerPage, currentPage);
+    console.log('current page', currentPage);
+    console.log('fetched posts', fetchedPosts);
 
-    if (!fetchedPosts || fetchedPosts.length === 0) {
+    // Check if fetchedPosts is null or undefined
+    if (!fetchedPosts) {
+      console.log('No posts returned.');
+      showMoreBtn.style.display = 'none'; // Hide the button
+      return; // Exit the function early if no posts are returned
+    }
+
+    if (fetchedPosts.length === 0) {
       console.log('No more posts available.');
+      showMoreBtn.style.display = 'none'; // Hide the button
       return;
     }
 
+    // If fewer posts than expected are fetched, it means we've reached the last page
+    if (fetchedPosts.length < postsPerPage) {
+      showMoreBtn.disabled = true; // Disable the button
+      showMoreBtn.style.cursor = 'disabled'; // Change the cursor to disabled
+    }
+
+    console.log('postssss', posts);
     posts = posts.concat(fetchedPosts); // Combine new posts with existing ones
     renderBlogCardsSet(fetchedPosts);
+
   } catch (error) {
     console.error('Error rendering blog cards:', error);
   } finally {
@@ -39,6 +58,7 @@ async function renderBlogCards() {
     loadedContent.style.display = "block";
   }
 }
+
 
 function renderBlogCardsSet(postsToRender) {
   const container = document.getElementById('blogCardContainer'); // Make sure to create this container in your HTML
